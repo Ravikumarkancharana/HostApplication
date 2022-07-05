62d27f33df5227d934948ae7c19502ec71142dd7
Ravikumarkancharana <ravikumarkancharana424@gmail.com>
2022-07-05 20:29:51 +0530

    Add project files.

---
+++ HostApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HostApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
